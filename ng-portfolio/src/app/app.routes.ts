8d3327e8475d20309fa39a04eb34f3d70d4020c4
Kaushal <kaushal2469@gmail.com>
2024-03-03 00:30:42 +0530

    home-page, about-page routing added

--- ng-portfolio/src/app/app.routes.ts
+++ ng-portfolio/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from '../stories/home-page/home-page.component';
+import { AboutPageComponent } from '../stories/about-page/about-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path:'',component:HomePageComponent},
+{path:'home',component:HomePageComponent},
+{path:'about',component:AboutPageComponent}];
+
+@NgModule({
+    imports:[RouterModule.forChild(routes)],
+    exports:[RouterModule]
+})
+export class AppRoutes{}
