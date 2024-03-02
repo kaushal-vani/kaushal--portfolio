ad02aba8bf4d7255e93b151b1cc186ca4f363e07
Kaushal <kaushal2469@gmail.com>
2024-03-02 19:24:47 +0530

    header, homepage, button implementation

--- ng-portfolio/src/app/app.component.ts
+++ ng-portfolio/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "../stories/header/header.component";
+import { HomePageComponent } from '../stories/home-page/home-page.component';
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HeaderComponent,HomePageComponent]
 })
 export class AppComponent {
   title = 'ng-portfolio';
