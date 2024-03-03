11293e9c3a7c1419286f375bcb08a4d53804e5ff
Kaushal <kaushal2469@gmail.com>
2024-03-04 01:09:39 +0530

    app component test coverage

--- ng-portfolio/src/app/app.component.spec.ts
+++ ng-portfolio/src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Acme');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      "Kaushal's Portfolio"
+    );
   });
 });
