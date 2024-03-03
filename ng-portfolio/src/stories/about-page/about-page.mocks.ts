c8821826081595c4958d9083e63eefe59138feab
Kaushal <kaushal2469@gmail.com>
2024-03-03 23:24:13 +0530

    about-page mocks models update

--- ng-portfolio/src/stories/about-page/about-page.mocks.ts
+++ ng-portfolio/src/stories/about-page/about-page.mocks.ts
@@ -1,9 +1,10 @@
 import type { Card } from './about-page.models';
 
 const AngularCardMock: Card = {
-  icon_path: '../../assets/storybook.svg',
+  icon_path: '../../assets/angular.svg',
   alt_text: 'Angular Card Logo',
   title: 'Angular',
+  meta: 'TypeScript-based open-source web application framework',
   description:
     'Angular is a powerful open-source web application framework for building dynamic, single-page applications with a focus on modular and declarative programming.',
 };
--- ng-portfolio/src/stories/about-page/about-page.models.ts
+++ ng-portfolio/src/stories/about-page/about-page.models.ts
@@ -2,5 +2,6 @@ export interface Card {
   icon_path: string;
   alt_text: string;
   title: string;
+  meta: string;
   description: string;
 }
