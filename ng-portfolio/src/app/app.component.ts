8e4474664caedf17e9a575052bcfc5b006747112
Kaushal <kaushal2469@gmail.com>
2024-03-03 18:46:58 +0530

    app component import changes

--- ng-portfolio/src/app/app.component.ts
+++ ng-portfolio/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../stories/header/header.component';
-import { HomePageComponent } from '../stories/home-page/home-page.component';
 import { FooterComponent } from '../stories/footer/footer.component';
 
 @Component({
@@ -9,7 +8,7 @@ import { FooterComponent } from '../stories/footer/footer.component';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, HeaderComponent, HomePageComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
 })
 export class AppComponent {
   title = 'ng-portfolio';
